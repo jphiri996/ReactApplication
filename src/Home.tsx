db6330414c4870818a4dd16cd0cdf5d7028be3c0
jphiri <jphiri@myune.edu.au>
2024-09-22 17:56:32 +1000

    Modify the home page to show details

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,31 @@
 //import React from 'react';
 //import logo from './logo.svg';
 import './Home.css';
+import movies from './data/movies.json';
 
 function Home() {
   return <>
+          <header className='header'>Movies App</header>
+          <main className='main'>
+            <div className='container'>
+              {movies.map((movie) => (
+              <div className='movie' key={movie.imdbID}>
+                <img className='poster' src={movie.Images[1]} alt={movie.Title} />
+                <div className='title'>{movie.Title}</div>
+              </div>
+              ))}
+              {/*
+              <ul className='movie-list'>
+              {movies.map((movie) => (
+                <li className='movie' key={movie.imdbID}>
+                  <div className='title'>{movie.Title}</div>
+                </li>
+              ))}
+            </ul>
+            */}
+            </div>
+
+          </main>
   
   </>
 }
